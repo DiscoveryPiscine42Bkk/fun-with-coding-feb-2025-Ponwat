41a6aa64be82defdc6496e946d98d23713869f35
ponwat <buddykung9000@gmail.com>
2025-02-08 13:58:48 +0700

    cell3.4

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,63 +1,37 @@
-const todo_container = document.querySelector('#ft_list');
-const button = document.querySelector('#new-todo');
+$(document).ready(function () {
+  $("#new-todo").click(function () {
+    const todo = prompt("Enter a new todo:");
+    if (todo === null || todo === "") {
+      return;
+    }
+    todos.push(todo);
+    $("#ft_list").prepend(`<div class="todo">${todo}</div>`);
+    const a = $(".todo");
+    console.log(a);
+  });
 
-let todo_list = [];
-let next_id = 0;
+  $("#ft_list").on("click", ".todo", function () {
+    if (confirm(`Do you want to delete ${$(this).text()}?`)) {
+      $(this).remove();
+    }
+  });
 
-if (document.cookie) {
-    const site_cookies = document.cookie.split('; ');
-    const todos_json = site_cookies
-        .map(cookie => cookie.split('=', 2))
-        .filter(cookie => cookie[0] === 'todos')[0][1];
-    const todo_texts = JSON.parse(todos_json);
-    todo_texts.forEach(todo_text => {
-        new_todo(todo_text);
+  $(window).on("beforeunload", function () {
+    const todos = $(".todo")
+      .map((_, element) => element.innerHTML)
+      .get()
+      .reverse();
+    const todos_json = JSON.stringify(todos);
+    document.cookie = `todos=${todos_json}`;
+  });
+
+  const todos_cookie = document.cookie
+    .split(";")
+    .find((cookie) => cookie.startsWith("todos="));
+  if (todos_cookie) {
+    todos = JSON.parse(todos_cookie.split("=")[1]);
+    todos.forEach((todo) => {
+      $("#ft_list").prepend(`<div class="todo">${todo}</div>`);
     });
-}
-
-button.onclick = () => {
-    const todo_text = prompt('What is your new todo?');
-    if (!todo_text) return;
-    new_todo(todo_text);
-}
-
-function new_todo(todo_text) {
-    todo = {
-        text: todo_text,
-        id: next_id++
-    }
-    todo_list.push(todo);
-
-
-    const todo_element = new_todo_element(todo);
-    todo_container.insertBefore(todo_element, todo_container.firstChild);
-
-    save_cookie();
-}
-
-function new_todo_element(todo) {
-    const new_todo = document.createElement('div');
-    new_todo.textContent = todo.text;
-    new_todo.id = todo.id;
-    new_todo.classList.add('todo');
-    new_todo.onclick = remove_todo(new_todo);
-    return new_todo;
-}
-
-function remove_todo(todo_element) {
-    return function() {
-        const confirm_delete = confirm(`Do you want to delete ${todo_element.textContent} ?`);
-        if (!confirm_delete) return;
-
-
-        todo_list = todo_list.filter(todo => todo.id != todo_element.id);
-        todo_container.removeChild(todo_element);
-
-        save_cookie();
-    }
-}   
-
-function save_cookie() {
-    const todo_texts = todo_list.map(todo => todo.text);
-    document.cookie = `todos=${JSON.stringify(todo_texts)}`;
-}
+  }
+});
